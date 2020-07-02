d7b2672859317b7d3b47982fc4ff192f6c801ec9
gid-code <gideonpeprah94@gmail.com>
2020-07-02 01:06:14 +0000

    error handling

--- leadmanager/frontend/src/actions/leads.js
+++ leadmanager/frontend/src/actions/leads.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
-
-import {GET_LEADS,DELETE_LEAD, ADD_LEAD} from './types'
+import { createMessage } from './messages'
+import {GET_LEADS,DELETE_LEAD, ADD_LEAD, GET_ERRORS} from './types'
 
 //get leads
 export const getLeads = () => dispatch =>{
@@ -11,29 +11,62 @@ export const getLeads = () => dispatch =>{
          payload: res.data
       })
    })
-   .catch(err => console.log(err))
+   .catch(err => {
+      const errors = {
+         msg: err.response.data,
+         status: err.response.status
+      }
+      dispatch({
+         type: GET_ERRORS,
+         payload: errors
+      })
+   })
 }
 
 //delete lead
 export const deleteLead = (id) => dispatch =>{
-   axios.delete(`/api/leads/${id}/`)
+   axios
+   .delete(`/api/leads/${id}/`)
    .then(res => {
+      dispatch(createMessage({ leadDeleted: 'Lead Deleted'}));
       dispatch({
          type: DELETE_LEAD,
          payload: id
       })
    })
-   .catch(err => console.log(err))
+   .catch(err => {
+      const errors = {
+         msg: err.response.data,
+         status: err.response.status
+      }
+      dispatch({
+         type: GET_ERRORS,
+         payload: errors
+      })
+   })
 }
 
 //add lead
 export const addLead = (lead) => dispatch =>{
-   axios.post('/api/leads/',lead)
+   axios
+   .post('/api/leads/',lead)
    .then(res => {
       dispatch({
          type: ADD_LEAD,
          payload: res.data
       })
+      dispatch(createMessage({
+         leadAdded: 'Lead Added'
+      }))
    })
-   .catch(err => console.log(err))
+   .catch(err => {
+      const errors = {
+         msg: err.response.data,
+         status: err.response.status
+      }
+      dispatch({
+         type: GET_ERRORS,
+         payload: errors
+      })
+   })
 }
--- leadmanager/frontend/src/components/App.js
+++ leadmanager/frontend/src/components/App.js
@@ -2,20 +2,33 @@ import React, {Component, Fragment} from 'react'
 import ReactDOM from 'react-dom'
 import Header from './layout/Header'
 import Dashboard from './leads/Dashboard'
+import Alert from './layout/Alert'
+
+import {positions, Provider as AlertProvider } from 'react-alert'
+import AlertTemplate from 'react-alert-template-basic'
 
 import {Provider} from 'react-redux'
 import store from '../store'
+
+//alert options
+const alertOptions = {
+   timeout: 3000,
+   position: positions.TOP_CENTER
+}
 
 class App extends Component{
    render() {
       return (
          <Provider store={store}>
+            <AlertProvider template={AlertTemplate} {...alertOptions}>
             <Fragment>
                <Header />
+               <Alert />
                <div className="container">
                   <Dashboard />
                </div>
-             </Fragment>
+            </Fragment>
+            </AlertProvider>
          </Provider>
          
       )
