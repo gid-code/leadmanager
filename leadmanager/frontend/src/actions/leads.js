c055f21b13bec50c91432c26169bc628b786edde
gid-code <gideonpeprah94@gmail.com>
2020-07-02 12:01:12 +0000

    token auth

--- leadmanager/frontend/src/actions/leads.js
+++ leadmanager/frontend/src/actions/leads.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
-import { createMessage } from './messages'
-import {GET_LEADS,DELETE_LEAD, ADD_LEAD, GET_ERRORS} from './types'
+import { createMessage, returnErrors } from './messages'
+import {GET_LEADS,DELETE_LEAD, ADD_LEAD} from './types'
 
 //get leads
 export const getLeads = () => dispatch =>{
@@ -11,16 +11,9 @@ export const getLeads = () => dispatch =>{
          payload: res.data
       })
    })
-   .catch(err => {
-      const errors = {
-         msg: err.response.data,
-         status: err.response.status
-      }
-      dispatch({
-         type: GET_ERRORS,
-         payload: errors
-      })
-   })
+   .catch(err => dispatch(returnErrors(
+      err.response.data, err.response.status
+   )))
 }
 
 //delete lead
@@ -34,16 +27,9 @@ export const deleteLead = (id) => dispatch =>{
          payload: id
       })
    })
-   .catch(err => {
-      const errors = {
-         msg: err.response.data,
-         status: err.response.status
-      }
-      dispatch({
-         type: GET_ERRORS,
-         payload: errors
-      })
-   })
+   .catch(err => dispatch(returnErrors(
+      err.response.data, err.response.status
+   )))
 }
 
 //add lead
@@ -59,14 +45,7 @@ export const addLead = (lead) => dispatch =>{
          leadAdded: 'Lead Added'
       }))
    })
-   .catch(err => {
-      const errors = {
-         msg: err.response.data,
-         status: err.response.status
-      }
-      dispatch({
-         type: GET_ERRORS,
-         payload: errors
-      })
-   })
+   .catch(err => dispatch(returnErrors(
+      err.response.data, err.response.status
+   )))
 }
