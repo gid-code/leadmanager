b1e660b5499e4de76765b7efcd306d361223079f
gid-code <gideonpeprah94@gmail.com>
2020-07-06 13:36:56 +0000

    authentication

--- leadmanager/frontend/src/components/App.js
+++ leadmanager/frontend/src/components/App.js
@@ -1,5 +1,6 @@
 import React, {Component, Fragment} from 'react'
 import ReactDOM from 'react-dom'
+import { HashRouter as Router,Route, Switch, Redirect} from 'react-router-dom'
 import Header from './layout/Header'
 import Dashboard from './leads/Dashboard'
 import Alert from './layout/Alert'
@@ -9,6 +10,10 @@ import AlertTemplate from 'react-alert-template-basic'
 
 import {Provider} from 'react-redux'
 import store from '../store'
+import Register from './accounts/Register'
+import Login from './accounts/Login'
+import PrivateRoute from './common/PrivateRoute'
+import { loadUser } from '../actions/auth'
 
 //alert options
 const alertOptions = {
@@ -17,17 +22,27 @@ const alertOptions = {
 }
 
 class App extends Component{
+   componentDidMount() {
+      store.dispatch(loadUser())
+   }
+   
    render() {
       return (
          <Provider store={store}>
             <AlertProvider template={AlertTemplate} {...alertOptions}>
-            <Fragment>
-               <Header />
-               <Alert />
-               <div className="container">
-                  <Dashboard />
-               </div>
-            </Fragment>
+               <Router>
+                  <Fragment>
+                     <Header />
+                     <Alert />
+                     <div className="container">
+                        <Switch>
+                           <PrivateRoute exact path="/" component={Dashboard} />
+                           <Route exact path="/register" component={Register} />
+                           <Route exact path="/login" component={Login} />
+                        </Switch>
+                     </div>
+                  </Fragment>
+               </Router>
             </AlertProvider>
          </Provider>
          
